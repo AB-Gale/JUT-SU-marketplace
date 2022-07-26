681244e13bae45001eb9d6f16a5fb2828ab36d35
AB-Gale <uran.aibashev.s@gmail.com>
2022-07-26 10:28:33 -0500

    Выполнено: UPDATE, DELETE, READ

--- src/contexts/AdminProvider.js
+++ src/contexts/AdminProvider.js
@@ -4,11 +4,25 @@ import { watchesApi } from "../helpers/const";
 export const AdminContext = React.createContext();
 
 const reducer = (state, action) => {
+  if (action.type === "GET_WATCHES") {
+    return {
+      ...state,
+      watches: action.payload,
+    };
+  } else if (action.type === "GET_WATCH_TO_EDIT") {
+    return {
+      ...state,
+      watchToEdit: action.payload,
+    };
+  }
   return state;
 };
 
 function AdminProvider({ children }) {
-  const [state, dispatch] = React.useReducer(reducer, {});
+  const [state, dispatch] = React.useReducer(reducer, {
+    watches: [],
+    watchToEdit: null,
+  });
 
   const sendNewWatch = (newWatch) => {
     fetch(watchesApi, {
@@ -20,9 +34,60 @@ function AdminProvider({ children }) {
     });
   };
 
-  const data = {
-    sendNewWatch,
+  const getWatches = () => {
+    fetch(watchesApi)
+      .then((res) => res.json())
+      .then((data) => {
+        let action = {
+          type: "GET_WATCHES",
+          payload: data,
+        };
+        dispatch(action);
+      });
   };
+
+  const deleteWatch = (id) => {
+    fetch(`${watchesApi}/${id}`, {
+      method: "DELETE",
+    }).then(() => getWatches());
+  };
+
+  // ! UPDATE - 1 Part
+
+  const getWatchToEdit = (id) => {
+    fetch(`${watchesApi}/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        let action = {
+          type: "GET_WATCH_TO_EDIT",
+          payload: data,
+        };
+        dispatch(action);
+      });
+  };
+
+  // ! UPDATE - 2 PART
+
+  const saveEditedWatch = (editedWatch) => {
+    fetch(`${watchesApi}/${editedWatch.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editedWatch),
+    });
+  };
+
+  const data = {
+    watches: state.watches,
+    watchToEdit: state.watchToEdit,
+    getWatches,
+    sendNewWatch,
+    deleteWatch,
+    getWatchToEdit,
+    saveEditedWatch,
+  };
+
   return <AdminContext.Provider value={data}>{children}</AdminContext.Provider>;
 }
 
