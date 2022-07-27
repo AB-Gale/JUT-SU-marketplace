c5374ad5621d20b35ec85818737b5042ba2445c8
AB-Gale <uran.aibashev.s@gmail.com>
2022-07-27 10:28:09 -0500

    Выполнено: READ(CLIENT), FILTER BY PRICE, LIVE SEARCH

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Slider,
+} from "@mui/material";
+import { ClientContext } from "../contexts/ClientProvider";
 
 function MainPage() {
+  const { getWatches, watches, setFilterByPrice, filterByPrice } =
+    React.useContext(ClientContext);
+
+  React.useEffect(() => {
+    getWatches();
+  }, [filterByPrice]);
   return (
-    <div>MainPage</div>
-  )
+    <div className="main-page">
+      <Container>
+        <h2>Entire Catalog</h2>
+        <div className="filter-block">
+          <h4>Filtration by Price</h4>
+          <Slider
+            max={10000}
+            min={0}
+            valueLabelDisplay="auto"
+            value={filterByPrice}
+            onChange={(e, newValue) => setFilterByPrice(newValue)}
+          />
+        </div>
+        <div className="products">
+          {watches.map((item) => (
+            <Card key={item.id} className="product-card">
+              <CardMedia component="img" height={140} image={item.photo} />
+              <CardContent>
+                <Typography
+                  className="product-card-title"
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  {item.name}
+                </Typography>
+                <ul className="product-card-ul">
+                  <li>
+                    <span>Brand:</span>
+                    <span>{item.brand}</span>
+                  </li>
+                  <li>
+                    <span>Release date:</span>
+                    <span>{item.year}</span>
+                  </li>
+                  <li>
+                    <span>Сountry of produce:</span>
+                    <span>{item.country}</span>
+                  </li>
+                  <li>
+                    <span>Price:</span>
+                    <span>{item.price}</span>
+                  </li>
+                </ul>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
